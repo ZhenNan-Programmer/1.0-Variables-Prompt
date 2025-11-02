10435c20b2c205a5fafa1bd8d75090f787c4d2de
Zhen Nan <lowzhennan@gmail.com>
2025-11-03 03:12:36 +0800

    deleted a line

--- 1VariableNConsolelog.js
+++ 1VariableNConsolelog.js
@@ -2,4 +2,3 @@ const name = "Bob"; //data type, variable name, variable value
 
 console.log(name); // printing job, get the variable ${name} in console
 // example changes here.
-// adding something here.
